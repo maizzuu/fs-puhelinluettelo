4d4e748313827b39779210403965ba165350290e
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-05-04 11:10:56 +0300

    3.13

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
 const app = express();
 
 morgan.token("content", function (req) {
@@ -16,39 +18,19 @@ app.use(
   )
 );
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-];
-
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((p) => p.id === id);
-  if (person) {
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
-app.get("/info", (request, response) => {
+/*app.get("/info", (request, response) => {
   const n = persons.length;
   const time = new Date();
   response.send(`<p>Phonebook has info for ${n} people</p><p>${time}</p>`);
@@ -59,11 +41,8 @@ app.delete("/api/persons/:id", (request, response) => {
   persons = persons.filter((p) => p.id !== id);
 
   response.status(204).end();
-});
+});*/
 
-const generateId = () => {
-  return Math.floor(Math.random() * 1000000);
-};
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
